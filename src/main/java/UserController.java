9b27473fc43cdd542e163322af56f47229754776
G00405014 <G00405014@atu.ie>
2023-10-17 14:14:35 +0100

    Added post Mapping and requestUserBody method

--- src/main/java/UserController.java
+++ src/main/java/UserController.java
@@ -1,7 +1,5 @@
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
@@ -12,5 +10,10 @@ public class UserController {
     @GetMapping("/registerUser/{name}/{email}")
     public String registerUser(@PathVariable String name, @PathVariable String email) {
         return userService.registerUser(name, email);
+    }
+
+    @PostMapping("/registerUserBody")
+    public String registerUserBody(@RequestBody user user) {
+        return userService.registerUser(user.getName(), user.getEmail());
     }
 }
